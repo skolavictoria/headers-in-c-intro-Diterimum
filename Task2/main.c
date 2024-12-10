6715c225b5ad08f1a6ad11efbc41dca6ab0cc7d7
Diterimum <154507451+Diterimum@users.noreply.github.com>
2024-12-10 21:33:17 +0000

    Yes it is

---
+++ Task2/main.c
@@ -0,0 +1,8 @@
+#include "math_operations.h"
+#include "print_operations.h"
+
+int main(){
+    int result=add(5, 10);
+    print_result(result);
+    return 0;
+}
---
+++ Task2/math_operations.c
@@ -0,0 +1,18 @@
+#include "math_operations.h"
+
+int add(int a, int b){
+    int result=a+b;
+    return result;
+}
+int subtract(int a, int b){
+    int result=a-b;
+    return result;
+}
+int divide(int a, int b){
+    int result=a/b;
+    return result;
+}
+int multiply(int a, int b){
+    int result=a*b;
+    return result;
+}
